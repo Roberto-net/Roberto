031997edfb32e30ea644390baf91b2984f88efc8
Vitaliy_K <spamer999@tut.by>
2019-12-10 21:20:33 +0300

    oop2

---
+++ OOP2/src/com/company/Dog.java
@@ -0,0 +1,42 @@
+package com.company;
+
+public class Dog extends Animal {
+    public String name;
+    //статические свойства располагабться в памяти в единственном экземляре
+    public static int footCount;
+    public Dog (){
+        super("green",9);
+        this.name = "rex";
+        Cat cat = new Cat();
+    }
+
+    public Dog (String color, int age, String name){
+        super(color,age);
+        this.name = name;
+    }
+    /*public void say (String sound){
+        System.out.println("haw-haw" + sound);
+    }*/
+    //классы находящиеся внутри других классов - inner class
+    public static class Cat {
+        public String name;
+        Cat() {
+           this.name="juniper";
+           name="bloodhound";
+        }
+    }
+    private class Mouse {
+        Mouse (){
+            name = "Jerry";
+        }
+    }
+    public static void bark(){
+        //this.name = "ыывй"; // ошибка
+        //для статического метода нельзя использовать ссылку на экземпляр объекта
+        System.out.println("woof-woof");
+        sleep();
+    }
+    public static void sleep (){
+        System.out.println("z-z-z-z-z-z");
+    }
+}
---
+++ OOP2/src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+       /* Animal animal = new Animal("white", 5);
+        System.out.println(animal.color);
+        System.out.println(animal.age);
+        //Animal animal2= new Animal ("black",10);
+        //System.out.println(animal2.color);
+        //System.out.println(animal2.color);*/
+        Dog dog = new Dog();
+        //обращение к статическим полям класса осуществляется через название класса
+        dog.footCount = 4;
+        System.out.println(dog.footCount);
+        System.out.println(dog.color);
+        System.out.println(dog.age);
+        Dog dog2 = new Dog("red", 7, "Rex");
+        System.out.println(dog2.footCount);
+        System.out.println(dog2.color);
+        System.out.println(dog2.age);
+        //dog2.say("uy uy");
+        Dog.Cat cat = new Dog.Cat();
+        cat.name = "Rikki";
+        Dog.bark();
+        dog.bark();
+
+
+
+    }
+}
